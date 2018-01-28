b65c8e673583edc9ebf7a4fe596e8f27bf17dc24
Andrew Lee <andrew1421lee@gmail.com>
2018-01-28 08:00:56 +0800

    talking to wegmans

--- serv/main.py
+++ serv/main.py
@@ -3,6 +3,8 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 from google.oauth2 import service_account
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -28,22 +30,6 @@ def index():
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
 	return jsonify({'tasks': tasks})
-
-@app.route('/test', methods=['GET'])
-def implicit():
-    from google.cloud import storage
-
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
-
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    
-    out = ""
-    for x in buckets:
-        out += x + "\n"
-    return out
     
 @app.route('/process_input/<string:text>', methods=['GET'])
 def entities_text(text):
@@ -76,8 +62,35 @@ def entities_text(text):
         #print(u'{:<16}: {}'.format('salience', entity.salience))
         #print(u'{:<16}: {}'.format('wikipedia_url',
         #      entity.metadata.get('wikipedia_url', '-')))
+    
     return out
     
+#@app.route('/get_auth', methods=['GET'])
+#def auth():
+#    post_url = 'https://login.microsoftonline.com/1318d57f-757b-45b3-b1b0-9b3c3842774f/oauth2/token'
+#    headers = {
+#        'content-type': 'application/x-www-form-urlencoded',
+#    }
+#    
+#
+#    r = requests.post(post_url, data=data, headers=headers)
+#    print(r.text)
+#    return r.text
+
+@app.route('/search/<string:query>', methods=['GET'])
+def search(query):
+    get_url = 'https://wegmans-es.azure-api.net/productpublic/products/search?criteria=' + query
+    data = json.load(open('WegmansKeys.json'))
+    wegmans_auth = data["access_token"]
+    primary_key = data["primary_key"]
+    headers = {
+        'product-subscription-key': primary_key,
+        'Authorization': "Bearer " + wegmans_auth
+    }
+
+    r = requests.get(get_url, headers=headers)
+    return r.text
+
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify({'error': '404: Bad time to get lost, FRIEND!'}), 404)
